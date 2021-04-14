98a980978742ab817d22ca84eab3f9e1f8c7098a
NamCheolKim <pkeowkd@naver.com>
2021-04-15 03:01:06 +0900

    SpringInAction

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -1,43 +1,46 @@
 package tacos.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tacos.domain.Ingredient;
+import tacos.domain.Order;
 import tacos.domain.Taco;
+import tacos.repository.IngredientRepository;
+import tacos.repository.TacoRepository;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepository;
+    private TacoRepository tacoRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.tacoRepository = tacoRepository;
+    }
+
     @GetMapping
-    public String showDesignForm(Model model){
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(i -> ingredients.add(i));
 
         Ingredient.Type[] types = Ingredient.Type.values();
-        for(Ingredient.Type type : types){
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
 
         model.addAttribute("taco", new Taco());
@@ -45,22 +48,33 @@ public class DesignTacoController {
         return "design";
     }
 
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type){
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Ingredient.Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
 
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors){
-        if(errors.hasErrors()){
+    public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
+        if (errors.hasErrors()) {
             return "design";
         }
 
-        log.info("Processing design:" + design);
+        Taco saved = tacoRepository.save(design);
+        order.addDesign(saved);
 
         return "redirect:/orders/current";
     }
-
 }
