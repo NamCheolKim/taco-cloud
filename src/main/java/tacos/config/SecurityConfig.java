aceede6188d068bcd900db571480f8de9c543d11
NamCheolKim <pkeowkd@naver.com>
2021-04-20 02:24:24 +0900

    SpringInAction

--- src/main/java/tacos/config/SecurityConfig.java
+++ src/main/java/tacos/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package tacos.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.security.NoEncodingPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -15,7 +19,13 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception { // HTTP 보안 구성 메소드
@@ -30,9 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // 사용자 인증 정보 구성 메소드
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from users where username = ?")
-                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?")
-        .passwordEncoder(new NoEncodingPasswordEncoder());
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
     }
 }
