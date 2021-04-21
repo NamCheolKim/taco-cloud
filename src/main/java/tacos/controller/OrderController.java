f598f60ba0e7a31b31c0475bf0e25b190e6d1315
NamCheolKim <pkeowkd@naver.com>
2021-04-22 04:22:45 +0900

    주문 폼에서 사용자 정보가 채워져 있도록 @AuthenticationPrincipal 어노테이션 추가 코드 수정

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -1,15 +1,16 @@
 package tacos.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.domain.Order;
+import tacos.domain.User;
 import tacos.repository.OrderRepository;
 
 import javax.validation.Valid;
@@ -27,7 +28,23 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute Order order) {
+
+        if(order.getDeliveryName() == null){
+            order.setDeliveryName(user.getFullName());
+        }
+        if(order.getDeliveryStreet() == null){
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if(order.getDeliveryCity() == null){
+            order.setDeliveryCity(user.getCity());
+        }
+        if(order.getDeliveryState() == null){
+            order.setDeliveryState(user.getState());
+        }
+        if(order.getDeliveryZip() == null){
+            order.setDeliveryZip(user.getZip());
+        }
         return "orderForm";
     }
 
@@ -36,6 +53,11 @@ public class OrderController {
         if(errors.hasErrors()){
             return "orderForm";
         }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        order.setUser(user);
 
         orderRepository.save(order);
         sessionStatus.setComplete();
